19ce2615adfe17490527dfc55dd662b3cec82936
3A532027 <s3a532027@student.nuct.edu.tw>
2018-11-10 00:09:50 +0800

    練習1_2

--- app/Http/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 2018/11/9
- * Time: 下午 11:30
- */
-Route::get('hello/{name?}',['as'=>'hello.index','uses' => 'HelloController@index']);
-
