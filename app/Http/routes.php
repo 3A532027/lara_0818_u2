c38d4c3eac2cdbb7f81388e2543214c81ad0c0be
3A532027 <s3a532027@student.nuct.edu.tw>
2018-11-09 23:34:02 +0800

    練習1_6

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,3 +5,6 @@
  * Date: 2018/11/9
  * Time: 下午 11:30
  */
+Route::get('home',function(){
+    return view('home.index');
+});
--- resources/views/home/index.php
+++ resources/views/home/index.php
@@ -3,5 +3,5 @@
  * Created by PhpStorm.
  * User: User
  * Date: 2018/11/9
- * Time: 下午 11:29
+ * Time: 下午 11:32
  */
