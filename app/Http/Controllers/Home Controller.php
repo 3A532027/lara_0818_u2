98eb39cf8a8745ff0fe563c20d75c53a177a14e0
3A532027 <s3a532027@student.nuct.edu.tw>
2018-11-09 23:59:58 +0800

    練習1_8

--- app/Http/Controllers/Home Controller.php
+++ app/Http/Controllers/Home Controller.php
@@ -5,3 +5,14 @@
  * Date: 2018/11/9
  * Time: 下午 11:58
  */
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class HomeController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('home.index');
+    }
+}
