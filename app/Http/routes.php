01891b212488133ef713eaf635d008c6f4e12a26
3A532027 <s3a532027@student.nuct.edu.tw>
2018-11-09 23:54:12 +0800

    練習3_1~3

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,11 +5,5 @@
  * Date: 2018/11/9
  * Time: 下午 11:30
  */
-class HomeController extends Controller
-{
-    public function index()
-    {
-        return view('home.index');
-    }
-}
+Route::get('hello/{name?}',['as'=>'hello.index','uses' => 'HelloController@index']);
 
---
+++ resources/views/Hello/hello.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+@section('title','Home')
+@section('content')
+    <h1>This is {{ $name }}'s home.</h1>
+@endsection
+
